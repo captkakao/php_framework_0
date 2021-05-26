07ae622e1b7796a6a0a64cf675be7558a1bfffa9
Sultanbek <soultouchka@gmail.com>
2021-05-26 12:13:32 +0600

    build: created Router class, with several methods

---
+++ vendor/core/Router.php
@@ -0,0 +1,38 @@
+<?php
+
+
+class Router
+{
+    protected static $routes = [];
+    protected static $route = [];
+
+    public static function add($regexp, $route) {
+        self::$routes[$regexp] = $route;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getRoutes()
+    {
+        return self::$routes;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getRoute()
+    {
+        return self::$route;
+    }
+
+    public static function matchRoute($url) {
+        foreach (self::$routes as $pattern => $route) {
+            if ($url === $pattern) {
+                self::$route = $route;
+                return true;
+            }
+        }
+        return false;
+    }
+}
