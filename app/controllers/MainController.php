ca3e64a9fb230e2d621c15fba65092025b62ac19
Sultanbek <soultouchka@gmail.com>
2021-05-31 02:02:46 +0600

    refactor: adding some methods to work with db

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -12,6 +12,10 @@ class MainController extends AppController
     {
         $model = new Main();
         $posts = $model->findAll();
+        $post = $model->findOne(2);
+        $data = $model->findBySql("SELECT * FROM posts ORDER BY id DESC LIMIT 2");
+        debug($data);
+//        debug($post);
 
         $this->setVars(compact('posts'));
     }
--- vendor/core/base/Model.php
+++ vendor/core/base/Model.php
@@ -9,6 +9,7 @@ abstract class Model
 {
     protected $pdo;
     protected $table;
+    protected $pk = 'id';
 
     public function __construct() {
         $this->pdo = Db::instance();
@@ -21,5 +22,21 @@ abstract class Model
     public function findAll() {
         $sql = "SELECT * FROM {$this->table}";
         return $this->pdo->query($sql);
+    }
+
+    public function findOne($id, $field = '') {
+        $field = $field ?: $this->pk;
+        $sql = "SELECT * FROM {$this->table} WHERE $field = ? LIMIT 1";
+        return $this->pdo->query($sql, [$id]);
+    }
+
+    public function findBySql($sql, $params = []) {
+        return $this->pdo->query($sql, $params);
+    }
+
+    public function findLike($str, $field, $table = '') {
+        $table = $table ?: $this->table;
+        $sql = "SELECT * FROM $table WHERE $field LIKE ?";
+        return $this->pdo->query($sql, ['%' . $str . '%']);
     }
 }
